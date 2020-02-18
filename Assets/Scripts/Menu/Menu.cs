5fa600955278f36fc7d0216e8ffa8404b113a6fd
sstese86 <sstese86@gmail.com>
2020-02-19 02:40:44 +0900

    Import Sprite Pakage.
    Setup Main Menu UI.
    
    Make Temp Icon and Images for testing.

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NaviEnt.UI
+{
+    public abstract class Menu<T>: MonoBehaviour where T: Menu<T>
+    {
+        static T _instance;
+        public static T Instance => _instance;
+        protected virtual void Awake()
+        {
+            if (_instance != null) Destroy(gameObject);
+            else _instance = this as T;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            _instance = null;
+        }
+        public static void MenuOpen()
+        {
+            Instance.gameObject.SetActive(true);
+        }
+
+        public static void OnBackPressed()
+        {
+            Instance.gameObject.SetActive(false);
+        }
+    }
+
+    //public abstract class Menu : MonoBehaviour
+    //{
+
+    //}
+}
