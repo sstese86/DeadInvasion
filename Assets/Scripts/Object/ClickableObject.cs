59a45a8631cba43db3704bee48baeb8327e2d871
sstese86 <sstese86@gmail.com>
2020-02-21 03:23:00 +0900

    3D Mainmap System Implemented.

---
+++ Assets/Scripts/Object/ClickableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Feedbacks;
+
+public class ClickableObject : MonoBehaviour
+{
+
+    public MMFeedbacks _mmFeedback;
+    
+    private void OnMouseDown()
+    {
+        Debug.Log("I'm Clicked!!");
+        _mmFeedback.PlayFeedbacks();
+    }
+}
